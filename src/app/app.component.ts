529a63d3fde10ca2fdc38889665100f496661164
Elencarlos Soares <elencarlos@uft.edu.br>
2019-02-04 14:34:52 -0300

    buscando fotos da api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Caelum Pic';
+    title = 'Caelum Pic';
+    listaFotos;
+
+    constructor(private api: HttpClient) {
+        api.get('http://localhost:3000/v1/fotos')
+            .subscribe(fotosApi => {
+                    this.listaFotos = fotosApi;
+                    console.log(this.listaFotos);
+                }, error => {
+                    console.log('Deu ruim');
+                }
+            );
+    }
 }
